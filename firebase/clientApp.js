f5f9f2b2f2b7dc3a78d0218a329f03e4bfdce999
Jacob Israel <65767293+Jake-salazar@users.noreply.github.com>
2022-07-04 22:36:11 +0800

    Update clientApp.js

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -3,6 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_API_KEY,
+//   authDomain: process.env.NEXT_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PROJECT_ID,
+//   storageBucket: process.env.NEXT_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_APP_ID,
+// };
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_API_KEY,
   authDomain: process.env.NEXT_AUTH_DOMAIN,
