f205f3b7046e394ebbb82c53215b0981bcce4db0
Jacob Israel <65767293+Jake-salazar@users.noreply.github.com>
2022-07-04 22:38:17 +0800

    Update clientApp.js

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -3,6 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDZCOOWJZl7O4JeOXZU6fOuxDZ-L9Pxwc4",
+  authDomain: "code-test-9525a.firebaseapp.com",
+  projectId: "code-test-9525a",
+  storageBucket: "code-test-9525a.appspot.com",
+  messagingSenderId: "124903919118",
+  appId: "1:124903919118:web:a66967d0cf748fc0e3765f",
+};
+
+// // Your web app's Firebase configuration
 // const firebaseConfig = {
 //   apiKey: process.env.NEXT_API_KEY,
 //   authDomain: process.env.NEXT_AUTH_DOMAIN,
@@ -11,16 +21,6 @@ import { getFirestore } from "firebase/firestore";
 //   messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
 //   appId: process.env.NEXT_APP_ID,
 // };
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.NEXT_API_KEY,
-  authDomain: process.env.NEXT_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PROJECT_ID,
-  storageBucket: process.env.NEXT_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_APP_ID,
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
